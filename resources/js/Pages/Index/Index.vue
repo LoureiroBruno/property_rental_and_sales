6a4a95dc3c8002fbb9c448d7109c3d54668c1aa5
Bruno Loureiro <brunoloureiro9@yahoo.com.br>
2025-05-07 14:07:07 -0300

    criado uma arquivo de layout padrão chamado MainLayout e sendo inserido nos componentes Index e Show .vue

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>Index</div>
-  <Link href="/hello">Show Page</Link>
+  <MainLayout> <!-- Alternatively, use <main-layout> (kebab-case) in templates -->
+    <div>Index</div>
+    <Link href="/hello">Show Page</Link>
 
-  <!-- Display the message passed from Laravel via props -->
-  <div>
-    The message is {{ message }}
-  </div>
+    <!-- Display the message passed from Laravel via props -->
+    <div>
+      The message is {{ message }}
+    </div>
+  </MainLayout>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3'
+import MainLayout from '@/Layouts/MainLayout.vue'
 
 // Define props with type validation (expects 'message' as a String)
 defineProps({

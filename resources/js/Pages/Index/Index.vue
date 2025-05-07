b56bccedb49f588525942b90f8cdf90ce7311b3b
Bruno Loureiro <brunoloureiro9@yahoo.com.br>
2025-05-07 10:41:28 -0300

    criado duas rotas de exibição e testado a chamada dos compontes dos metodos index e show

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,11 +1,8 @@
 <template>
-  Hello! {{ counter }}
+  <div>Index</div>
+  <Link href="/hello">Show Page</Link>
 </template>
 
 <script setup>
-import {ref} from 'vue'
-
-const counter = ref(0)
-setInterval(() => counter.value++, 1000)
-
+import { Link } from '@inertiajs/vue3' 
 </script>

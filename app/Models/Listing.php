3acf739d636987b00a17a88d96afabe5fcbe294a
Bruno Loureiro <brunoloureiro9@yahoo.com.br>
2025-05-09 12:14:09 -0300

    definidos quais campos podem ser usados nos metodos de criação e atualização

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,5 +9,30 @@ class Listing extends Model
 {
     use HasFactory;
 
-    // TODO
+    /**
+     * The attributes that are mass assignable.
+     *
+     * This defines which fields can be automatically filled
+     * when using methods like Model::create() or Model::update().
+     *
+     * For security, Laravel protects models against the mass assignment
+     * of unauthorized fields. Only the attributes listed here
+     * can be assigned in bulk operations.
+     *
+     * Example usage:
+     * Model::create([
+     *     'name' => 'João',
+     *     'email' => 'joao@email.com'
+     * ]);
+     */
+    protected $fillable = [
+        'beds',
+        'baths',
+        'area',
+        'city',
+        'code',
+        'street',
+        'street_nr',
+        'price'
+    ];
 }

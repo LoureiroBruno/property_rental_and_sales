38fd4a5f4912d7a9213142e271a6e01b55fb71af
Bruno Loureiro <brunoloureiro9@yahoo.com.br>
2025-05-10 11:33:15 -0300

    refatorado as configuração do handleInertiaRequest, na tratativa de erros, warning e infos

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,8 +39,11 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'flash' => [
-                'success' => $request->session()->get('success')
-            ]
+                'success' => fn() => $request->session()->get('success'),
+                'error'   => fn() => $request->session()->get('error'),
+                'warning' => fn() => $request->session()->get('warning'),
+                'info'    => fn() => $request->session()->get('info'),
+            ],
         ]);
     }
 }
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,9 +1,15 @@
 <template>
   <Link href="/">Main Page</Link>&nbsp;
   <Link href="/hello">Show Page</Link>
-  <div v-if="flashSuccess" class="success">
-    {{ flashSuccess }}
+
+  <div v-if="flash.success" class="alert alert-success"> <!--  if not empty it displays the message -->
+    {{ flash.success }}
   </div>
+
+  <div v-if="flash.error" class="alert alert-danger">
+    {{ flash.error }}
+  </div>
+
   <slot>Default</slot>
 </template>
 
@@ -11,14 +17,24 @@
 import { computed } from 'vue'
 import { Link, usePage } from '@inertiajs/vue3'
 
-const flashSuccess = computed(
-  () => usePage().props.flash.success,
-)
+// Pegamos os flashs de forma reativa
+const page = usePage()
+
+const flash = computed(() => page.props.flash)
 </script>
 
 <style scoped>
-.success {
+.alert-success {
   background-color: green;
   color: white;
+  padding: 1em;
+  margin-bottom: 1em;
+}
+
+.alert-danger {
+  background-color: red;
+  color: white;
+  padding: 1em;
+  margin-bottom: 1em;
 }
 </style>

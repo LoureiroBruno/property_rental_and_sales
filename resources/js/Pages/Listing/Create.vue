764b0bf06b7327173b9110c3e61d476507cd6d1d
Bruno Loureiro <brunoloureiro9@yahoo.com.br>
2025-05-13 19:41:49 -0300

    inserido os alerts de mensagens de erro

--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -4,7 +4,7 @@
             <div class="col-span-2">
                 <label class="label">Beds</label>
                 <input v-model.number="form.beds" type="text" class="input" />
-                <div v-if="form.errors.beds">
+                <div v-if="form.errors.beds" class="input-error">
                     {{ form.errors.beds }}
                 </div>
             </div>
@@ -12,14 +12,14 @@
             <div class="col-span-2">
                 <label class="label">Baths</label>
                 <input v-model.number="form.baths" type="text" class="input" />
-                <div v-if="form.errors.baths">
+                <div v-if="form.errors.baths" class="input-error">
                     {{ form.errors.baths }}
                 </div>
             </div>
             <div class="col-span-2">
                 <label class="label">Area</label>
                 <input v-model.number="form.area" type="text" class="input" />
-                <div v-if="form.errors.area">
+                <div v-if="form.errors.area" class="input-error">
                     {{ form.errors.area }}
                 </div>
             </div>
@@ -27,7 +27,7 @@
             <div class="col-span-4">
                 <label class="label">City</label>
                 <input v-model="form.city" type="text" class="input" />
-                <div v-if="form.errors.city">
+                <div v-if="form.errors.city" class="input-error">
                     {{ form.errors.city }}
                 </div>
             </div>
@@ -35,7 +35,7 @@
             <div class="col-span-2">
                 <label class="label">Post Code</label>
                 <input v-model="form.code" type="text" class="input" />
-                <div v-if="form.errors.code">
+                <div v-if="form.errors.code" class="input-error">
                     {{ form.errors.code }}
                 </div>
             </div>
@@ -43,7 +43,7 @@
             <div class="col-span-4">
                 <label class="label">Street</label>
                 <input v-model="form.street" type="text" class="input" />
-                <div v-if="form.errors.street">
+                <div v-if="form.errors.street" class="input-error">
                     {{ form.errors.street }}
                 </div>
             </div>
@@ -51,7 +51,7 @@
             <div class="col-span-2">
                 <label class="label">Street Nr</label>
                 <input v-model.number="form.street_nr" type="text" class="input" />
-                <div v-if="form.errors.street_nr">
+                <div v-if="form.errors.street_nr" class="input-error">
                     {{ form.errors.street_nr }}
                 </div>
             </div>
@@ -59,7 +59,7 @@
             <div class="col-span-6">
                 <label class="label">Price</label>
                 <input v-model.number="form.price" type="text" class="input" />
-                <div v-if="form.errors.price">
+                <div v-if="form.errors.price" class="input-error">
                     {{ form.errors.price }}
                 </div>
             </div>

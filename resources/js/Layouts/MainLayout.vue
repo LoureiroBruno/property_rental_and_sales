0134f8a4f394a8a292797d4f37b967c113042fde
Bruno Loureiro <brunoloureiro9@yahoo.com.br>
2025-05-13 12:26:53 -0300

    instalado o compomente ziggy-js e importado as classes do framework tailwind nas views do componentes MainLayout e app.blade

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,16 +1,34 @@
 <template>
-  <Link href="/listing">Listings</Link>&nbsp;
-  <Link href="/listing/create">New Listing</Link>
+  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+    <div class="container mx-auto">
+      <nav class="p-4 flex items-center justify-between">
+        <div class="text-lg font-medium">
+          <Link :href="route('listing.index')">Listings</Link>
+        </div>
+        <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+          <Link :href="route('listing.index')">Property Rental and Sales</Link>
+        </div>
+        <div>
+          <Link :href="route('listing.create')"
+            class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing</Link>
+        </div>
+      </nav>
+    </div>
+  </header>
 
-  <div v-if="flash.success" class="alert alert-success"> <!--  if not empty it displays the message -->
-    {{ flash.success }}
-  </div>
+  <main class="container mx-auto p-4">
+    <div v-if="flash.success"
+      class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+      {{ flash.success }}
+    </div>
 
-  <div v-if="flash.error" class="alert alert-danger">
-    {{ flash.error }}
-  </div>
+    <div v-if="flash.error"
+      class="mb-4 border rounded-md shadow-sm border-red-200 dark:border-red-800 bg-red-50 dark:bg-red-900 p-2">>
+      {{ flash.error }}
+    </div>
 
-  <slot>Default</slot>
+    <slot>Default</slot>
+  </main>
 </template>
 
 <script setup>

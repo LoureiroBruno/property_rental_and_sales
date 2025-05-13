5762516c20ec7dbd18b815922905d50a65c1f96a
Bruno Loureiro <brunoloureiro9@yahoo.com.br>
2025-05-13 15:35:16 -0300

    instalado o componente @heroicons/vue e inserido na view index listing

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -8,13 +8,33 @@
                 <ListingAddress :listing="listing" class="text-gray-500" />
                 </Link>
             </div>
-            <div>
-                <Link :href="route('listing.edit', { listing: listing.id })">Edit</Link>
-            </div>
-            <div>
-                <Link :href="route('listing.destroy', { listing: listing.id })" method="DELETE" as="button">Delete
+
+            <!-- <div class="mt-4 flex gap-2">
+                <Link :href="route('listing.edit', { listing: listing.id })"
+                    class="px-4 py-1 text-sm bg-blue-500 text-white rounded hover:bg-blue-800 transition">
+                Editar
+                </Link>
+
+                <Link :href="route('listing.destroy', { listing: listing.id })" method="DELETE" as="button"
+                    class="px-2 py-1 text-sm bg-red-500 text-white rounded hover:bg-red-800 transition">
+                Deletar
+                </Link>
+            </div> -->
+
+            <div class="mt-4 flex gap-2">
+                <Link :href="route('listing.edit', { listing: listing.id })"
+                    class="flex items-center gap-1 px-4 py-1 text-sm bg-blue-500 text-white rounded hover:bg-blue-800 transition">
+                <PencilSquareIcon class="w-4 h-4" />
+                Editar
+                </Link>
+
+                <Link :href="route('listing.destroy', { listing: listing.id })" method="DELETE" as="button"
+                    class="flex items-center gap-1 px-2 py-1 text-sm bg-red-500 text-white rounded hover:bg-red-800 transition">
+                <TrashIcon class="w-4 h-4" />
+                Deletar
                 </Link>
             </div>
+
         </Box>
     </div>
 </template>
@@ -25,6 +45,7 @@ import ListingAddress from '@/Components/ListingAddress.vue'
 import Box from '@/Components/UI/Box.vue'
 import ListingSpace from '@/Components/ListingSpace.vue'
 import Price from '@/Components/Price.vue'
+import { PencilSquareIcon, TrashIcon } from '@heroicons/vue/24/solid'
 defineProps({
     listings: Array,
 })

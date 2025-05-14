c48f75e00837cde5aa51b9fe8d4286bba190d03d
Bruno Loureiro <brunoloureiro9@yahoo.com.br>
2025-05-13 21:21:09 -0300

    inserido o icone de fechar no flash messeger

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -19,12 +19,16 @@
 
   <main class="container mx-auto p-4">
     <div v-if="flash.success"
-      class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+      class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2 relative">
+      <button @click="closeMessage('success')" class="absolute top-2 right-2 text-lg text-green-600">
+        <XMarkIcon class="w-8 h-7" />
+      </button>
       {{ flash.success }}
     </div>
 
     <div v-if="flash.error"
-      class="mb-4 border rounded-md shadow-sm border-red-200 dark:border-red-800 bg-red-50 dark:bg-red-900 p-2">>
+      class="mb-4 border rounded-md shadow-sm border-red-200 dark:border-red-800 bg-red-50 dark:bg-red-900 p-2 relative">
+      <button @click="closeMessage('error')" class="absolute top-2 right-2 text-lg text-red-600"> ✖ </button>
       {{ flash.error }}
     </div>
 
@@ -35,12 +39,16 @@
 <script setup>
 import { computed } from 'vue'
 import { Link, usePage } from '@inertiajs/vue3'
-import { ListBulletIcon } from '@heroicons/vue/24/solid'
+import { ListBulletIcon, XMarkIcon } from '@heroicons/vue/24/solid'
 
 // Pegamos os flashs de forma reativa
 const page = usePage()
 
 const flash = computed(() => page.props.flash)
+
+const closeMessage = (type) => {
+  flash.value[type] = '' // clear in mensseger (success ou error)
+}
 </script>
 
 <style scoped>

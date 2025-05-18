28c13032a7c90a14ab49f368d5abddcfc3c42855
Bruno Loureiro <brunoloureiro9@yahoo.com.br>
2025-05-18 14:52:07 -0300

    criado politica de grupo aos metods e add ao controller listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ListingController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Listing::class, 'listing');
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -20,37 +26,14 @@ class ListingController extends Controller
             'areaFrom',
             'areaTo'
         ]);
-        $query = Listing::orderByDesc('created_at');
-
-        if ($filters['priceFrom'] ?? false) {
-            $query->where('price', '>=', $filters['priceFrom']);
-        }
-
-        if ($filters['priceTo'] ?? false) {
-            $query->where('price', '<=', $filters['priceTo']);
-        }
-
-        if ($filters['beds'] ?? false) {
-            $query->where('beds', $filters['beds']);
-        }
-
-        if ($filters['baths'] ?? false) {
-            $query->where('baths', $filters['baths']);
-        }
-
-        if ($filters['areaFrom'] ?? false) {
-            $query->where('area', '>=', $filters['areaFrom']);
-        }
-
-        if ($filters['areaTo'] ?? false) {
-            $query->where('area', '<=', $filters['areaTo']);
-        }
 
         return inertia(
             'Listing/Index',
             [
                 'filters' => $filters,
-                'listings' => $query->paginate(9)
+                'listings' => Listing::mostRecent()
+                    ->filter($filters)
+                    ->paginate(9)
                     ->withQueryString()
             ]
         );
@@ -61,6 +44,7 @@ class ListingController extends Controller
      */
     public function create()
     {
+        // $this->authorize('create', Listing::class);
         return inertia('Listing/Create');
     }
 

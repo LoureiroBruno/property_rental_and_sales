d55ff4dd9de4586c733c24a3f9b6c644b7a0de66
Bruno Loureiro <brunoloureiro9@yahoo.com.br>
2025-05-19 19:24:01 -0300

    refatorado arquivo de rotas e views

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,10 +3,8 @@
     class="sticky top-0 z-50 mb-1 w-full border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 shadow-sm dark:shadow-gray-800">
     <div class="container mx-auto">
       <nav class="p-4 flex flex-wrap items-center justify-between gap-4 md:flex-nowrap">
-
-        <!-- Menu Esquerdo (visível se usuário autenticado) -->
-        <div v-if="user">
-          <div class="relative flex items-center gap-2 text-lg font-medium text-slate-100">
+        <div v-if="user" class="flex flex-col sm:flex-row items-center justify-between gap-4 w-full px-4">
+          <div class="flex items-center gap-4 text-lg font-medium text-slate-100">
             <Link :href="route('listing.index')"
               class="flex items-center gap-2 font-bold text-gray-700 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-400">
             <Squares2X2Icon class="w-6 h-6" />
@@ -19,71 +17,91 @@
             New Listing
             </Link>
           </div>
-        </div>
 
-        <!-- Centro (Título / Logo) -->
-        <div class="flex-1 text-center font-bold text-xl text-gray-800 dark:text-gray-200">
-          <Link :href="route('listing.index')">Property Rental and Sales</Link>
-        </div>
+          <div class="flex-1 text-center font-bold text-xl text-gray-800 dark:text-gray-200">
+            <Link :href="route('listing.index')">Property Rental and Sales</Link>
+          </div>
 
-        <!-- Menu Direita (usuário logado) -->
-        <div v-if="user" class="relative flex items-center gap-6">
-          <div id="user-menu" class="relative">
-            <button @click="toggleDropdown"
-              class="flex items-center gap-2 px-4 py-2 bg-stone-100 dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-md shadow-lg z-50 overflow-hidden">
-              <ListBulletIcon class="w-6 h-6" />
-              <span class="text-sm font-medium text-gray-700 dark:text-gray-200">{{ user.name }}</span>
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-              </svg>
-            </button>
+          <div class="relative flex items-center gap-6">
 
-            <!-- Dropdown Menu -->
-            <div v-if="open"
-              class="absolute right-0 mt-2 w-full bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-md shadow-lg z-50 overflow-hidden">
-              <div id="tester" class="w-full justify-center">
+            <div id="user-menu" class="relative">
+              <button @click="toggleDropdown"
+                class="flex items-center gap-2 px-4 py-2 bg-stone-100 dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-md shadow-lg z-50 overflow-hidden">
+                <ListBulletIcon class="w-6 h-6" />
+                <span class="text-sm font-medium text-gray-700 dark:text-gray-200">{{ user.name }}</span>
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24"
+                  stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                </svg>
+              </button>
 
-                <!-- Dark / Light Mode Toggle -->
-                <button @click="toggleTheme(); closeDropdownClick()"
-                  class="w-full flex items-center justify-start gap-1 px-4 py-2 pl-8 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800">
-                  <component :is="isDark ? SunIcon : MoonIcon" :class="isDark ? 'w-8 h-7' : 'w-8 h-6'" />
-                  <span>{{ isDark ? 'Switch to Light Mode' : 'Switch to Dark Mode' }}</span>
-                </button>
+              <!-- Dropdown Menu -->
+              <div v-if="open"
+                class="absolute right-0 mt-2 w-56 bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-md shadow-lg z-50 overflow-hidden">
+                <div id="tester" class="w-full justify-center">
 
-                <div class="h-px bg-gray-200 dark:bg-gray-700 my-1"></div>
+                  <!-- Dark / Light Mode Toggle -->
+                  <button @click="toggleTheme(); closeDropdownClick()"
+                    class="w-full flex items-center justify-start gap-1 px-4 py-2 pl-8 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800">
+                    <component :is="isDark ? SunIcon : MoonIcon" :class="isDark ? 'w-8 h-7' : 'w-8 h-6'" />
+                    <span>{{ isDark ? 'Switch to Light Mode' : 'Switch to Dark Mode' }}</span>
+                  </button>
 
-                <!-- Edit Profile Button -->
-                <button @click="closeDropdownClick"
-                  class="w-full flex items-center justify-start gap-1 px-4 py-2 pl-8 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800">
-                  <UserCircleIcon class="w-8 h-6" />
-                  <span>My Access Profile</span>
-                </button>
+                  <div class="h-px bg-gray-200 dark:bg-gray-700 my-1"></div>
 
+                  <!-- Edit Profile Button -->
+                  <button @click="closeDropdownClick"
+                    class="w-full flex items-center justify-start gap-1 px-4 py-2 pl-8 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800">
+                    <UserCircleIcon class="w-8 h-6" />
+                    <span>My Access Profile</span>
+                  </button>
 
-                <div class="h-px bg-gray-200 dark:bg-gray-700 my-1"></div>
+                  <div class="h-px bg-gray-200 dark:bg-gray-700 my-1"></div>
 
-                <!-- Realtor Profile Link -->
-                <Link :href="route('realtor.listing.index')"
-                  class="w-full flex items-center justify-start gap-1 px-4 py-2 pl-8 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800"
-                  @click="closeDropdownClick">
-                <BuildingOfficeIcon class="w-8 h-6" />
-                <span>Real Estate Agent</span>
+                  <!-- Realtor Profile Link -->
+                  <Link :href="route('realtor.listing.index')"
+                    class="w-full flex items-center justify-start gap-1 px-4 py-2 pl-8 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800"
+                    @click="closeDropdownClick">
+                  <BuildingOfficeIcon class="w-8 h-6" />
+                  <span>Real Estate Agent</span>
+                  </Link>
+
+                  <div class="h-px bg-gray-200 dark:bg-gray-700 my-1"></div>
+                </div>
+
+                <!-- Logout -->
+                <Link :href="route('logout')" method="delete" as="button"
+                  class="w-full flex items-center justify-center gap-1 px-4 py-2 text-sm text-white bg-[indianred] hover:bg-[firebrick] transition">
+                <ArrowLeftOnRectangleIcon class="w-8 h-6" />
+                <span>Logout</span>
                 </Link>
-
-                <div class="h-px bg-gray-200 dark:bg-gray-700 my-1"></div>
               </div>
-
-              <!-- Logout -->
-              <Link :href="route('logout')" method="delete" as="button"
-                class="w-full flex items-center justify-center gap-1 px-4 py-2 text-sm text-white bg-[indianred] hover:bg-[firebrick] transition">
-              <ArrowLeftOnRectangleIcon class="w-8 h-6" />
-              <span>Logout</span>
-              </Link>
             </div>
           </div>
         </div>
 
+
+        <div v-else class="flex flex-col sm:flex-row items-center justify-between gap-4 w-full px-4">
+          <div
+            class="flex items-center gap-2 text-lg font-medium text-slate-900 border border-gray-300 rounded-md px-2 py-1 dark:text-slate-100 dark:border-gray-600">
+            <Link :href="route('user-account.create')"
+              class="flex items-center gap-2 font-bold text-gray-700 dark:text-gray-300 hover:text-indigo-600 dark:hover:text-indigo-400">
+            <PlusIcon class="w-6 h-6" />
+            Register
+            </Link>
+          </div>
+
+          <div class="flex-1 text-center font-bold text-xl text-gray-800 dark:text-gray-200">
+            <Link :href="route('listing.index')">Property Rental and Sales</Link>
+          </div>
+
+          <div class="flex items-center gap-6">
+            <Link :href="route('login')" class="btn-primary flex items-center gap-1">
+            <ArrowRightOnRectangleIcon class="w-5 h-5" />
+            <span>Sign In</span>
+            </Link>
+          </div>
+        </div>
       </nav>
     </div>
   </header>
@@ -125,6 +143,7 @@ import {
   PlusIcon,
   ListBulletIcon,
   UserCircleIcon,
+  ArrowRightOnRectangleIcon,
   ArrowLeftOnRectangleIcon
 } from '@heroicons/vue/24/solid'
 
